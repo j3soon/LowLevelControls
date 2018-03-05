3fa97188267840057c2cc2f37368d432a1c0f12d
Johnson <j3.soon@msa.hinet.net>
2018-03-05 14:00:12 +0800

    Add Keyboard simulation

--- LowLevelControls.Sample/Program.cs
+++ LowLevelControls.Sample/Program.cs
@@ -13,74 +13,8 @@ namespace LowLevelControls.Sample
 
         static void Main(string[] args)
         {
-            kbdHook.NativeHookProcEvent += (nCode, wParam, lParam) =>
-            {
-                KBDLLHOOKSTRUCT kbd =
-                    (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
-                switch ((WM)wParam)
-                {
-                    case WM.KEYDOWN:
-                    case WM.SYSKEYDOWN:
-                        Console.WriteLine((Keys)kbd.vkCode + " Down");
-                        break;
-                    case WM.KEYUP:
-                    case WM.SYSKEYUP:
-                        Console.WriteLine((Keys)kbd.vkCode + " Up");
-                        break;
-                }
-                return IntPtr.Zero;
-            };
-
-            msHook.NativeHookProcEvent += (nCode, wParam, lParam) =>
-            {
-                MSLLHOOKSTRUCT ms =
-                    (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
-                switch ((WM)wParam)
-                {
-                    case WM.LBUTTONDOWN:
-                        Console.WriteLine(Keys.LButton + $" Down on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.LBUTTONUP:
-                        Console.WriteLine(Keys.LButton + $" Up on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.MOUSEMOVE:
-                        Console.WriteLine($"Mouse Move to ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.MOUSEWHEEL:
-                        Console.WriteLine($"Mouse Wheel with data {HookBase.HighWord(ms.mouseData)} on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.MOUSEHWHEEL:
-                        //Not implemented yet.
-                        break;
-                    case WM.RBUTTONDOWN:
-                        Console.WriteLine(Keys.RButton + $" Down on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.RBUTTONUP:
-                        Console.WriteLine(Keys.RButton + $" Up on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.MBUTTONDOWN:
-                        Console.WriteLine(Keys.MButton + $" Down on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.MBUTTONUP:
-                        Console.WriteLine(Keys.MButton + $" Up on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.XBUTTONDOWN:
-                        if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON1)
-                            Console.Write(Keys.XButton1);
-                        else if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON2)
-                            Console.Write(Keys.XButton2);
-                        Console.WriteLine($" Down on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                    case WM.XBUTTONUP:
-                        if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON1)
-                            Console.Write(Keys.XButton1);
-                        else if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON2)
-                            Console.Write(Keys.XButton2);
-                        Console.WriteLine($" Up on ({ms.pt.x}, {ms.pt.y})");
-                        break;
-                }
-                return IntPtr.Zero;
-            };
+            kbdHook.NativeHookProcEvent += KbdHook_NativeHookProcEvent;
+            msHook.NativeHookProcEvent += MsHook_NativeHookProcEvent;
             AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
             kbdHook.InstallGlobalHook();
             msHook.InstallGlobalHook();
@@ -89,6 +23,75 @@ namespace LowLevelControls.Sample
                 Thread.Sleep(1);
                 Application.DoEvents();
             }
+        }
+
+        private static IntPtr KbdHook_NativeHookProcEvent(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            KBDLLHOOKSTRUCT kbd =
+                (KBDLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(KBDLLHOOKSTRUCT));
+            switch ((WM)wParam)
+            {
+                case WM.KEYDOWN:
+                case WM.SYSKEYDOWN:
+                    Console.WriteLine((Keys)kbd.vkCode + " Down");
+                    break;
+                case WM.KEYUP:
+                case WM.SYSKEYUP:
+                    Console.WriteLine((Keys)kbd.vkCode + " Up");
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private static IntPtr MsHook_NativeHookProcEvent(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            MSLLHOOKSTRUCT ms =
+                (MSLLHOOKSTRUCT)Marshal.PtrToStructure(lParam, typeof(MSLLHOOKSTRUCT));
+            switch ((WM)wParam)
+            {
+                case WM.LBUTTONDOWN:
+                    Console.WriteLine(Keys.LButton + $" Down on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.LBUTTONUP:
+                    Console.WriteLine(Keys.LButton + $" Up on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.MOUSEMOVE:
+                    Console.WriteLine($"Mouse Move to ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.MOUSEWHEEL:
+                    Console.WriteLine($"Mouse Wheel with data {HookBase.HighWord(ms.mouseData)} on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.MOUSEHWHEEL:
+                    //Not implemented yet.
+                    break;
+                case WM.RBUTTONDOWN:
+                    Console.WriteLine(Keys.RButton + $" Down on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.RBUTTONUP:
+                    Console.WriteLine(Keys.RButton + $" Up on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.MBUTTONDOWN:
+                    Console.WriteLine(Keys.MButton + $" Down on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.MBUTTONUP:
+                    Console.WriteLine(Keys.MButton + $" Up on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.XBUTTONDOWN:
+                    if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON1)
+                        Console.Write(Keys.XButton1);
+                    else if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON2)
+                        Console.Write(Keys.XButton2);
+                    Console.WriteLine($" Down on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+                case WM.XBUTTONUP:
+                    if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON1)
+                        Console.Write(Keys.XButton1);
+                    else if (HookBase.HighWord(ms.mouseData) == (int)MOUSEDATA.XBUTTON2)
+                        Console.Write(Keys.XButton2);
+                    Console.WriteLine($" Up on ({ms.pt.x}, {ms.pt.y})");
+                    break;
+            }
+            return IntPtr.Zero;
         }
 
         private static void CurrentDomain_ProcessExit(object sender, EventArgs e)
---
+++ LowLevelControls/Keyboard.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using LowLevelControls.Natives;
+
+namespace LowLevelControls
+{
+    public static class Keyboard
+    {
+        [DllImport("user32.dll")]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        static extern short GetKeyState(int nVirtKey);
+
+        public static void SendKey(int vKey)
+        {
+            INPUT[] inputs = new INPUT[2];
+            inputs[0].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[0].ki = new KEYBDINPUT
+            {
+                wVk = (ushort)vKey,
+                wScan = 0,
+                dwFlags = (uint)KEYEVENTF.KEYDOWN,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+            inputs[1].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[1].ki = new KEYBDINPUT
+            {
+                wVk = (ushort)vKey,
+                wScan = 0,
+                dwFlags = (uint)KEYEVENTF.KEYUP,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+            if (SendInput(2, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        public static void SendKeyDown(int vKey)
+        {
+            INPUT[] inputs = new INPUT[1];
+            inputs[0].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[0].ki = new KEYBDINPUT
+            {
+                wVk = (ushort)vKey,
+                wScan = 0,
+                dwFlags = (uint)KEYEVENTF.KEYDOWN,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+            if (SendInput(1, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        public static void SendKeyUp(int vKey)
+        {
+            INPUT[] inputs = new INPUT[1];
+            inputs[0].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[0].ki = new KEYBDINPUT
+            {
+                wVk = (ushort)vKey,
+                wScan = 0,
+                dwFlags = (uint)KEYEVENTF.KEYUP,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+            if (SendInput(1, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        public static bool IsKeyDown(int vKey)
+        {
+            return (GetKeyState(vKey) & 0x80) != 0;
+        }
+
+        public static bool IsKeyToggled(int vKey)
+        {
+            return (GetKeyState(vKey) & 0x01) != 0;
+        }
+
+        public static void SendChar(char c)
+        {
+            INPUT[] inputs = new INPUT[1];
+            inputs[0].type = INPUTTYPE.INPUT_KEYBOARD;
+            inputs[0].ki = new KEYBDINPUT
+            {
+                wVk = 0,
+                wScan = c,
+                dwFlags = (uint)KEYEVENTF.UNICODE,
+                time = 0,
+                dwExtraInfo = IntPtr.Zero
+            };
+            if (SendInput(1, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+
+        public static void SendText(String text)
+        {
+            int len = text.Length;
+            INPUT[] inputs = new INPUT[len];
+            for (int i = 0; i < len; i++)
+            {
+                inputs[i].type = INPUTTYPE.INPUT_KEYBOARD;
+                inputs[i].ki = new KEYBDINPUT
+                {
+                    wVk = 0,
+                    wScan = text[i],
+                    dwFlags = (uint)KEYEVENTF.UNICODE,
+                    time = 0,
+                    dwExtraInfo = IntPtr.Zero
+                };
+            }
+            if (SendInput((uint)len, inputs, Marshal.SizeOf(typeof(INPUT))) == 0)
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+        }
+    }
+}
