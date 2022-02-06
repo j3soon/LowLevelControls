5349e430b2e0c711c4e4060a42b12d86abf5be08
Johnson <j3.soon@msa.hinet.net>
2022-02-06 16:25:43 +0800

    Fix x64 alignment issue (#1)

--- LowLevelControls/Natives/INPUT.cs
+++ LowLevelControls/Natives/INPUT.cs
@@ -7,11 +7,21 @@ namespace LowLevelControls.Natives
     {
         [FieldOffset(0)]
         public INPUTTYPE type;
+        // Deal with difference in x86/x64 packing
+        #if WIN64
+        [FieldOffset(8)]
+        public MOUSEINPUT mi;
+        [FieldOffset(8)]
+        public KEYBDINPUT ki;
+        [FieldOffset(8)]
+        public HARDWAREINPUT hi;
+        #else
         [FieldOffset(4)]
         public MOUSEINPUT mi;
         [FieldOffset(4)]
         public KEYBDINPUT ki;
         [FieldOffset(4)]
         public HARDWAREINPUT hi;
+        #endif
     }
 }
