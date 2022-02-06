4a35a395b3e9e31b3def64c0182d1bf96bcf9a48
Johnson <j3.soon@msa.hinet.net>
2022-02-06 16:33:18 +0800

    Add SendText sample

---
+++ LowLevelControls.Sample3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LowLevelControls.Sample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(3000);
+            Keyboard.SendText("ABCDEFG");
+        }
+    }
+}
